4e122aa29e08839034e7c69eb28d1c44352d8300
shubham88fru <shubham88fru@yahoo.com>
2024-05-23 16:33:23 -0700

    fixup

--- grpc-playground/src/main/java/org/shubham/sec03/Lec01Scalar.java
+++ grpc-playground/src/main/java/org/shubham/sec03/Lec01Scalar.java
@@ -1,12 +1,13 @@
 package org.shubham.sec03;
 
+import org.shubham.models.sec03.PersonOuterClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Lec01Scalar {
     private static final Logger log = LoggerFactory.getLogger(Lec01Scalar.class);
     public static void main(String[] args) {
-        var person = org.shubham.models.sec03.Person.newBuilder()
+        var person = PersonOuterClass.Person.newBuilder()
                 .setLastName("sam")
                 .setAge(12)
                 .setEmail("sam@gmail.com")
