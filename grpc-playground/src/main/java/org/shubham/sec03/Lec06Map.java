097c2309f954c6094d3a6a587d6df46a65b5b909
shubham88fru <shubham88fru@yahoo.com>
2024-05-24 21:02:55 -0700

    maps

---
+++ grpc-playground/src/main/java/org/shubham/sec03/Lec06Map.java
@@ -0,0 +1,30 @@
+package org.shubham.sec03;
+
+import org.shubham.models.sec03.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lec06Map {
+
+    private static final Logger logger = LoggerFactory.getLogger(Lec06Map.class);
+
+    public static void main(String[] args) {
+        var car1 = Map.Car.newBuilder()
+                .setMake("Honda")
+                .setModel("civic")
+                .setYear(2000)
+                .build();
+
+        var car2 = Map.Car.newBuilder()
+                .setMake("Honda")
+                .setModel("Accord")
+                .setYear(2002)
+                .build();
+
+        var dealer = Map.Dealer.newBuilder()
+                .putInventory(car1.getYear(), car1)
+                .putInventory(car2.getYear(), car2).build();
+
+        logger.info("{}", dealer);
+    }
+}
